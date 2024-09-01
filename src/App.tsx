5fcba9305067d798b78e5c80eb1efe77f655f0bb
大沼 峻徳 <numa.san.1024@gmail.com>
2024-09-01 18:36:48 +0900

    JSONファイルをアップロードして読み込むこともできるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
+import { ICruiseResult } from "dependency-cruiser";
 import { FC, useState, useMemo, useCallback } from "react";
 import { Box, Stack, TextField, Typography } from "@mui/material";
 import { clamp } from "lodash-es";
+import { z } from "zod";
 
-import depcruiseResult from "./debug/dependency-result.json";
+import sampleDepcruiseResult from "./debug/dependency-result.json";
 import { DepcruiseGraph } from "./components/DepcruiseGraph";
 import type { DepcruiseGraphOption } from "./components/DepcruiseGraph";
 import { DebugDotGraph } from "./components/DebugDotGraph";
 import { PathBreadCrumbs } from "./components/PathBreadCrumbs";
 
+const depcruiseResultSchema = z.object({
+  modules: z.array(
+    z.object({
+      source: z.string(),
+      dependencies: z.array(
+        z.object({
+          dynamic: z.boolean(),
+          module: z.string(),
+          moduleSystem: z.string(),
+          dependencyTypes: z.array(z.string()),
+          resolved: z.string(),
+        })
+      ),
+      dependents: z.array(z.string()),
+      orphan: z.boolean(),
+      valid: z.boolean(),
+    })
+  ),
+  summary: z.any(),
+});
+
 export const App: FC = () => {
+  const [depcruiseResult, setDepcruiseResult] = useState<ICruiseResult>(
+    () => sampleDepcruiseResult as any
+  );
   const [baseUrl, setBaseUrl] = useState(
     "https://github.com/TakanoriOnuma/depcruise-visualizer/tree/main"
   );
@@ -28,6 +54,26 @@ export const App: FC = () => {
   return (
     <Box sx={{ p: 2 }}>
       <Stack spacing={1}>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Typography>File: </Typography>
+          <input
+            type="file"
+            onChange={(event) => {
+              const file = event.target.files?.[0];
+              if (file == null) {
+                return;
+              }
+              const reader = new FileReader();
+              reader.onload = () => {
+                const json = JSON.parse(reader.result as string);
+                depcruiseResultSchema.parse(json);
+                // パースに成功したらセットする
+                setDepcruiseResult(json);
+              };
+              reader.readAsText(file);
+            }}
+          />
+        </Stack>
         <Stack direction="row" alignItems="center" spacing={1}>
           <Typography>BaseUrl: </Typography>
           <TextField
